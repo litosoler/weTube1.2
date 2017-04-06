3fa8cd9735c1acfcb12159345b9e9c9cb0f0baed
litosoler <lito.soler1@gmail.com>
2017-04-05 18:46:22 -0600

    cambio url del css
    como le habia cambiado el nombre a los archivos css no los estaba
    leyendo

--- crearCuenta.php
+++ crearCuenta.php
@@ -6,7 +6,7 @@
 		  <meta charset="utf-8">
 		  <meta name="viewport" content="width=device-width, initial-scale=1">
 		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		  <link rel="stylesheet" type="text/css" href="css/pagina-crearCuenta.css">
+		  <link rel="stylesheet" type="text/css" href="css/css-crearCuenta.css">
 	
 	</head>
 
