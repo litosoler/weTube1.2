907a194b50035949236149cfe13051ec5f0b7ea5
jelsyn <jelsync@gmail.com>
2017-04-10 14:50:48 -0600

    Solo actualice la validacion

--- uNr-crearCuenta.php
+++ uNr-crearCuenta.php
@@ -12,7 +12,7 @@
 
 	<body>
 		<div class="container-fluid" id= "encabezado"">
-			<a href="uNr-inicio.php"><img src="img/logo-youtube.png" id="img-1"></a>
+			<a href=""><img src="img/logo-youtube.png" id="img-1"></a>
 			<a href="ur-iniciarSesion.php" class="btn btn-primary" id="btn-1" role="button">Acceder</a>
 		</div>
 
@@ -26,62 +26,62 @@
 
 			 <div class="form-group col-xs-12 col-sm-12 col-md-6">
 			 		
-			 					<div class="form-group">
+			 					<div id="div-usr" class="form-group">
 								  <label for="usr">Nombre:</label>
 								  <input type="text" class="form-control" id="usr">
 								</div>
 
 			 					
 			 				
-			 					<div class="form-group">
+			 					<div id="div-email" class="form-group">
 								    <label for="email">Correo Electronico:</label>
 								    <input type="email" class="form-control" id="email">
 								</div>
 								<span class="help-block"><a href="#">Me gustaría tener una nueva dirección de Gmail</a></span>
 			 				
-					 			<div class="form-group">
+					 			<div id="div-pwd" class="form-group">
 									  <label for="pwd">Crea tu contraseña:</label>
 									  <input type="password" class="form-control" id="pwd">
 								</div>
 			 				
-			 					<div class="form-group">
-									  <label for="pwd-2">Confirmar la contraseña:</label>
-									  <input type="password" class="form-control" id="pwd-2">
+			 					<div id="div-pwd2" class="form-group">
+									  <label for="pwd2">Confirmar la contraseña:</label>
+									  <input type="password" class="form-control" id="pwd2">
 								</div>
 			 					<div class="form-group">
 			 					 <label for="fecha">Fecha de Nacimiento:</label>
-			 					<input type="date" name="fecha" class="form-control" >
+			 					<input type="date" id="fecha" class="form-control" >
 			 					<span class="text-danger"> No puedes dejar este campo en blanco</span>
 			 					</div>
 			 					<div class="form-group">
 									  <label for="genero">Genero:</label>
 									 <select class="form-control" id="genero">
-									 	<option>Soy....</option>
-									    <option>Hombre</option>
-									    <option>Mujer</option>
-									    <option>Otro</option>
-									    <option>Prefiero no decir</option>
+									 	<option value="" >Soy....</option>
+									    <option value="1" >Hombre</option>
+									    <option value="2" >Mujer</option>
+									    <option value="3" >Otro</option>
+									    <option value="4" >Prefiero no decir</option>
 								  </select>
 								 </div>
 				 				
-			 					<div class="form-group">
+			 					<div id="div-telefono" class="form-group">
 			 					 <label for="telefono">Teléfono celular:</label>
 			 					 <button class="btn btn-info "></button>
-			 					<input type="phone" name="telefono" class="form-control" value="+504" >
+			 					<input type="phone" id="telefono" class="form-control" value="" placeholder="+504" >
 			 					</div>
-			 				
+			 					
 								<label for="ubicacion">Ubicación:</label>
 								<select class="form-control" id="ubicacion">
-									 	<option>Soy de..</option>
-									    <option>Honduras</option>
-									    <option>Guatemala</option>
-									    <option>El Salvador</option>
-									    <option>Prefiero no decir</option>
+									 	<option selected="selected" value="" >Soy de..</option>
+									    <option value="1" >Honduras</option>
+									    <option value="2" >Guatemala</option>
+									    <option value="3" >El Salvador</option>
+									    <option value="4" >Prefiero no decir</option>
 								</select>
 
 						
 				 			
-				 					 <input type="submit" name="siguiente" class="btn btn-primary  btn-2" value="siguiente">
+				 					 <button id="btn-siguiente" class="btn btn-primary  btn-2" > Siguiente </button> 
 				 				
 			 </div>
 
@@ -90,6 +90,7 @@
 
 		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+		  <script src="js/crearCuenta.js"> </script>
+		  
 	</body>
 </html>
